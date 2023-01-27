a3cad034fb871624943b973f44288578c40762d4
JBrandon Duck-Mayr <j.duckmayr@gmail.com>
2023-01-27 15:37:02 -0600

    Add dataframe listing all monsters with full data

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' Monster List
+#'
+#' A list of the Monster Rancher 2 monsters the `ranchr` package has data on
+#'
+#' @format ## `monsterlist`
+#' A data frame with 337 rows and 3 columns:
+#' \describe{
+#'   \item{monster}{The name of this monster breed}
+#'   \item{main}{The main breed of this monster}
+#'   \item{sub}{The sub breed of this monster}
+#' }
+"monsterlist"
